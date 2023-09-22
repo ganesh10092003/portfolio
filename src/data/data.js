75909c75eaaa02c95b86d84cd3ca13a824287cd4
Ganesh <ch.tejamr@gmail.com>
2023-09-22 18:56:19 +0530

    added descriptions to projects

--- src/data/data.js
+++ src/data/data.js
@@ -1,11 +1,12 @@
 import eBlood from "../assets/eBlood.png"
 import portfolio from "../assets/portFolio.png"
+import insta_clone from "../assets/insta_clone.png"
 export const data = [
   {
     id: 1,
-    img: '',
+    img: insta_clone,
     title: "Instagram Clone",
-    desc: "",
+    desc: "Designed a replicate of the account page of Instagram. Started my journey in Web Development, and specifically gained experience in usage of flexbox, throughout the development process.",
     link: "https://ganesh10092003.github.io/insta_clone/",
     git: "https://github.com/ganesh10092003/insta_clone",
   },
@@ -13,7 +14,7 @@ export const data = [
     id: 2,
     img: eBlood,
     title: "Online Blood Donation",
-    desc: "",
+    desc: "We designed an online platform for booking blood donations; created a user‐friendly interface that enables blood banks and donors to create profiles and book donation slots. Using React.js I developed the Login Page of the website. During this journey I have learnt a lot about React.js and how useful it is for Web Developers.",
     link: "https://eblood.onrender.com/",
     git: "https://github.com/ganesh10092003/eBlood",
   },
@@ -21,8 +22,8 @@ export const data = [
     id: 3,
     img: portfolio,
     title: "PortFolio",
-    desc: "",
-    link: "",
+    desc: "Explore my portfolio site, meticulously crafted with ReactJS and Tailwind CSS. It not only showcases my work but also represents my unwavering commitment to producing elegant, user-centric web interfaces. This project highlights my technical proficiency and design finesse.",
+    link: "https://cstganesh.vercel.app/",
     git: "https://github.com/ganesh10092003/portfolio",
   }
 ];
